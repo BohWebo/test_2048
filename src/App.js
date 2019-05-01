35fa460640bcacf932d0ab95a9481453fbcef1a5
Webbo <Webbo@tuta.io>
2019-05-01 18:39:40 +0300

    game field was added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import Header from './components/Header'
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );  
+
+import Header from './components/Header';
+import Field from './components/Field/Field';
+
+
+
+class App extends React.Component {
+ 
+    state = {
+      cells: [
+        {
+          id:1,
+          x:0,
+          y:3,
+          value: 2,
+        }
+      ],
+    }
+  
+  render() {
+    const { cells } = this.state;
+    return (
+      <div className="App">
+        <Header />
+        <Field cells={cells} />
+      </div>
+    );  
+  
+  }
 }
 
 export default App;
